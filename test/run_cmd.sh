1bc72067bf7305d13808b72ff2c54252d8e149b6
bogyo210 <nbors@renderedrext.com>
2020-03-20 12:15:20 +0200

    bash -c instead of eval

--- test/run_cmd.sh
+++ test/run_cmd.sh
@@ -8,7 +8,7 @@ for line in $(cat $1);
 do
   echo "$line"
   #ssh -p29920 -i ~/.ssh/id_dsa -oStrictHostKeyChecking=false -oUserKnownHostsFile=/dev/null -oLogLevel=ERROR runner@$IP  "source .bash_profile ; source ~/.toolbox/sem-version ; $line"
-  eval $line
+  bash -c $line
   #scp  -i ~/.ssh/id_dsa -oStrictHostKeyChecking=false -oUserKnownHostsFile=/dev/null -P29920 runner@$IP  /tmp/cmd.log ./
   #cat cmd.log
 done
